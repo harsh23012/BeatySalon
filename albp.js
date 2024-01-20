5aa404fddddbe8df2af2ed42a492ed5dfd4c48e5
harsh23012 <125751156+harsh23012@users.noreply.github.com>
2024-01-20 20:18:30 +0530

    Add files via upload
    here is a basic beuty salon page

---
+++ albp.js
@@ -0,0 +1,10 @@
+var typed = new Typed('#multiple-text',{
+    strings: ['Hair Cutting', 'Makeup','Eybrow Making', 'Massage','Pedicure','Manicure','Mehandi'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true  
+});
+
+let service = document.querySelectorAll('.booking .input-box #service');
+let sub_service = document.querySelectorAll('.booking .input-box #sub-service')
